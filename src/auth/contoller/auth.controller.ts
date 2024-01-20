c518c3357f6ba6c5f36d789ca5870404f322c849
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-01-19 23:12:28 -0300

    subindo swagger

--- src/auth/contoller/auth.controller.ts
+++ src/auth/contoller/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { AuthRequest } from '../models/AuthRequest';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('usuario')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,12 +1,15 @@
-import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNumber, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUsuarioDto {
 
+    @ApiProperty()
     @IsString()
     nome?: string;
     @IsEmail()
     email?: string;
 
+    @ApiProperty()
     @IsString()
     @MinLength(4)
     @MaxLength(20)
@@ -14,14 +17,19 @@ export class CreateUsuarioDto {
       message: 'password too weak',
     })
 
+    @ApiProperty()
     @IsString()
     senha?: string;
     
+    @ApiProperty()
+    @IsNumber()
     idade?: Number;
 
+    @ApiProperty()
     @IsString()
     loja?: string;
 
+    @ApiProperty()
     @IsString()
     permissao?: string ;
 }
